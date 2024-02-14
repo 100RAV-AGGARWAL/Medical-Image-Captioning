ceb36a9e2166d3494abdb4e13e08417cee898a30
PadamBhardwaj <padambhardwaj100@gmail.com>
2024-02-14 09:19:39 +0530

    upload-result

---
+++ front-end/src/components/Upload/Result.tsx
@@ -0,0 +1,12 @@
+const Result = ({ status }: { status: string }) => {
+    if (status === "success") {
+        return <p>✅ File uploaded successfully!</p>;
+    } else if (status === "fail") {
+        return <p>❌ File upload failed!</p>;
+    } else if (status === "uploading") {
+        return <p>⏳ Uploading selected file...</p>;
+    } else {
+        return null;
+    }
+};
+export default Result;
--- front-end/src/components/Upload/Upload.tsx
+++ front-end/src/components/Upload/Upload.tsx
@@ -1,41 +1,68 @@
 import React, { useState } from "react";
-
+import Result from "./Result";
 const Uploader = () => {
-  const [file, setFile] = useState<File | null>(null);
+    const [file, setFile] = useState<File | null>(null);
+    const [status, setStatus] = useState<
+    "initial" | "uploading" | "success" | "fail"
+  >("initial");
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            setStatus("initial");
+            setFile(e.target.files[0]);
+        }
+    };
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      setFile(e.target.files[0]);
-    }
-  };
+    const handleUpload = async () => {
+        // API CALL
+        if (file) {
+            setStatus("uploading");
+            console.log("Uploading file...");
 
-  const handleUpload = async () => {
-    // API CALL
-    console.log("Successful operation");
-  };
+            const formData = new FormData();
+            formData.append("file",     file);
+            console.log(file)
+            try {
+                const result = await fetch("", { // API URL here
+                    method: "POST",
+                    body: formData,
+                });
 
-  return (
-    <>
-      <div>
-        <label htmlFor="file" className="sr-only">
-          Choose a file
-        </label>
-        <input id="file" type="file" onChange={handleFileChange} />
-      </div>
-      {file && (
-        <section>
-          File details:
-          <ul>
-            <li>Name: {file.name}</li>
-            <li>Type: {file.type}</li>
-            <li>Size: {file.size} bytes</li>
-          </ul>
-        </section>
-      )}
+                const data = await result.json();
 
-      {file && <button onClick={handleUpload}>Upload a file</button>}
-    </>
-  );
+                console.log(data);
+                console.log("Successful operation");
+                setStatus("success");
+            } catch (error) {
+                console.error(error);
+                setStatus("fail");
+            }
+        }
+
+    };
+
+    return (
+        <>
+            <div>
+                <label htmlFor="file" className="sr-only">
+                    Choose a file
+                </label>
+                <input id="file" type="file" onChange={handleFileChange} />
+            </div>
+            {file && (
+                <section>
+                    File details:
+                    <ul>
+                        <li>Name: {file.name}</li>
+                        <li>Type: {file.type}</li>
+                        <li>Size: {file.size} bytes</li>
+                    </ul>
+                </section>
+            )}
+
+            {file && <button onClick={handleUpload}>Upload a file</button>}
+            <Result status={status} />
+        </>
+    );
 };
 
 export default Uploader;
